3cad3efb0dacce9fb9a80f1c3011d04b6ce474db
navyaaaS1309 <navyasingla1309git@gmail.com>
2021-10-16 18:14:19 +0530

    completed volunteers styling

--- detect/src/pages/volunteer.js
+++ detect/src/pages/volunteer.js
@@ -8,6 +8,8 @@ class Volunteer extends React.Component{
 		this.state = {
 		  full_name : '',
 		  email: '',
+		  reason:'',
+		  image:'',
 		};
 	  }
 	  handleInputChange = (event) => {
@@ -18,8 +20,8 @@ class Volunteer extends React.Component{
 	  }
 	  onSubmit = (event) => {
 		event.preventDefault();
-		const { full_name,email } = this.state;
-		const data= {full_name,email}
+		const { full_name,email,reason,image } = this.state;
+		const data= {full_name,email,reason,image}
 		console.log(JSON.stringify(data))
         fetch('http://localhost:3001/volunteer', { 
             method:'POST',
@@ -45,6 +47,9 @@ class Volunteer extends React.Component{
 	    <form onSubmit={this.onSubmit}>
 	      <input type="text" className="fadeIn second" placeholder="Full Name" name="full_name"  onChange={this.handleInputChange}/>
 	      <input type="text" className="fadeIn third" placeholder="Email" name="email"  onChange={this.handleInputChange}/>
+		  <input type="text" className="fadeIn third" placeholder="Add an image url!" name="image"  onChange={this.handleInputChange}/>
+		  <textarea className="fadeIn third" placeholder="Why do you want to volunteer" name="reason"  onChange={this.handleInputChange}></textarea>
+		  
 	      <input type="submit" className="fadeIn fourth" value="Add my Info!"/>
 	    </form>   
 	  
--- detect/src/pages/volunteerlist.js
+++ detect/src/pages/volunteerlist.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import '../components/css/vols.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import doublequote from './style/doublequote.png'
+
 
 const Volunteers=()=>{
 
@@ -12,15 +15,25 @@ React.useEffect(() => {
       });
   }, []);
   return(
+    <div className="vols">
+         <h1>Our Volunteers</h1>
     <div className="vol-grid">
+   
         {
+          
             vol.map((volunteer)=>{
-              return( <div className="single-vol"><h1 style={{color:"black"}}>{volunteer.full_name?volunteer.full_name:"Mark jefferson"}</h1>
-                <button className="btn btn-primary rounded-pill"><a href={`mailto:${volunteer.email}`}>Connect over Mail!</a></button>
+              return( <div className="single-vol card">
+                <h1 style={{color:"black"}}>{volunteer.full_name?volunteer.full_name:"Mark jefferson"}</h1>
+                <img className="vol-img" src={volunteer.image}/>
+                <br/>
+                <img className="double-quote" src={doublequote}/>
+                <h5 style={{color:"black"}}> {volunteer.reason}</h5>
+                <button className="btn btn-dark rounded-pill"><a href={`mailto:${volunteer.email}`}>Connect over Mail!</a></button>
                 </div>
               )
             })
         }
+    </div>
     </div>
   );
 }
