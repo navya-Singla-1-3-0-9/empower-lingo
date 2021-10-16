b20106c9226ce65fdae9d32136dfe8a135b6d0fa
navyaaaS1309 <navyasingla1309git@gmail.com>
2021-10-16 12:27:13 +0530

    completed auth with passport

--- detect/src/pages/login.js
+++ detect/src/pages/login.js
@@ -1,17 +1,51 @@
 import React from "react";
 import '../components/css/login.css'
-const Login=()=>{
+
+
+class Login extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+		  username : '',
+		  password: '',
+		};
+	  }
+	  handleInputChange = (event) => {
+		const { value, name } = event.target;
+		this.setState({
+		  [name]: value
+		});
+	  }
+	  onSubmit = (event) => {
+		event.preventDefault();
+		const { username, password } = this.state;
+		const data= {username, password}
+		console.log(JSON.stringify(data))
+       fetch('http://localhost:3001/login', { 
+		   method:'POST',
+		   
+		   headers:{'Accept': 'application/json',"Content-Type":"application/json"},
+		   body:JSON.stringify(data)
+		   })   .then(response => response.json())
+		   .then(json => console.log(json))
+		   .catch(error => console.log('Authorization failed : ' + error.message));
+          
+	  }
+	
+
+
+	render(){
     return(
-      
+      <div className="login">
             <div className="container fadeInDown">
 	  <div id="formContent">
 	
 	    <h2 className="active"> Sign In </h2>
 	    <h2 className="inactive underlineHover">Sign Up</h2>
 	  
-	    <form>
-	      <input type="text" className="fadeIn second" placeholder="Username"/>
-	      <input type="password" className="fadeIn third" placeholder="Password"/>
+	    <form onSubmit={this.onSubmit}>
+	      <input type="text" className="fadeIn second" placeholder="Username" name="username"  onChange={this.handleInputChange}/>
+	      <input type="password" className="fadeIn third" placeholder="Password" name="password"  onChange={this.handleInputChange}/>
 	      <input type="submit" className="fadeIn fourth" value="Log In"/>
 	    </form>   
 	  
@@ -20,9 +54,9 @@ const Login=()=>{
 	    </div>
 	  </div>
 	</div>
- 
+	</div>
     
     );
-
+	}
 }
 export default Login
--- server/index.js
+++ server/index.js
@@ -1,18 +1,61 @@
 const express = require("express");
-
+const cors = require('cors')
+const corsOptions = {
+  origin: 'http://localhost:3000',
+}
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+var bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash');
+const passport= require('passport');
+const localStrategy= require('passport-local'); 
+app.use(bodyParser.urlencoded({ extended: false }));           
+app.use(bodyParser.json())
+const configuredCors = cors(corsOptions);
+
+app.options('*', configuredCors)
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
+  const sessionConfig={
+    secret: 'Thisisasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+      httpOnly: true,
+      expires: Date.now()+1000*60*60*24*7,
+      maxAge: 1000*60*60*24*7
+    }
+  }
+  const  User = require("./models/userschema");
+  app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+//handling login
+passport.serializeUser(User.serializeUser());
+//handling logout
+passport.deserializeUser(User.deserializeUser());
+const  mongoose  = require("mongoose");
+const url= 'mongodb://localhost:27017/asl'
+const  connect  =  mongoose.connect(url, { useNewUrlParser: true  });
+
+app.post('/login',configuredCors,passport.authenticate('local',{successRedirect:""}),(req,res)=>{
+ console.log(req.user);
+ res.json({success:"user authenticated"})
+});
+
+app.post('/register',configuredCors,async (req,res)=>{
+  const {email, username, password}= req.body;
+	const nu = new User({email, username, role:"patient"});
+	const regdUser= await User.register(nu, password);
+});
+
   
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
-});
-app.get("/translate" ,(req,res)=>{
-  res.sendFile(__dirname+ "/views/englishToasl.html");
-});
-app.get("/translate1" ,(req,res)=>{
-  res.sendFile(__dirname+ "/views/aslToEnglish.html");
 });
