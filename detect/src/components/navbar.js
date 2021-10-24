616cb8fb24db3b4ebeb91f7d76d264d1b919722e
navyaaaS1309 <navyasingla1309git@gmail.com>
2021-10-24 19:09:18 +0530

    make navbar responsive

--- detect/src/components/navbar.js
+++ detect/src/components/navbar.js
@@ -1,58 +1,101 @@
 import React,{Component} from "react";
 import './css/nav.css'
 import { Route, Switch, Link, BrowserRouter } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-class Navbar extends Component {
-  componentDidUpdate() {
-    let header = document.querySelector("h1");
-    let text = header.textContent.toLowerCase();
-    let color;
-    switch(text) {
-      case 'about':
-        color = "#16A085";
-        break;
-      case 'projects':
-        color = "#E74C3C";
-        break;
-      case 'contacts':
-        color = "#2980B9";
-        break;
-      default:
-        color = "#F1C40F";
-        break;
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class NavBar extends Component {
+  constructor(){
+    super();
+    this.state={
+      isOpen:false
     }
-    // update the custom property for the root element and have it cascade on the header as well
-    let root = document.querySelector(":root");
-    root.style.setProperty("--color-home", color);
   }
-
- 
-  render() {
-    return (
-      <div className="App">
-
-        <nav className="Navbar">
-          <h2>ASL</h2>
-          <ul>
-            <li>
-              <Link to="/about">about</Link>
-            </li>
-            <li>
-              <Link to="/projects">projects</Link>
-            </li>
-            <li>
-              <Link to="/contacts">contacts</Link>
-            </li>
-          </ul>
-        </nav>
-
-    
-
-      </div>
-    );
-  }
-	
+// when the component is updated (which also occurs as the new routes are included)
+// consider the text of the main header and alter the CSS custom property to style the background of the header in the navigation bar and the color of the header according to which route is included
+componentDidUpdate() {
+let header = document.querySelector("h1");
+let text = header.textContent.toLowerCase();
+let color;
+switch(text) {
+case 'about':
+  color = "#16A085";
+  break;
+case 'projects':
+  color = "#E74C3C";
+  break;
+case 'contacts':
+  color = "#2980B9";
+  break;
+default:
+  color = "#F1C40F";
+  break;
+}
+// update the custom property for the root element and have it cascade on the header as well
+let root = document.querySelector(":root");
+root.style.setProperty("--color-home", color);
 }
 
+/* render 
+- a nav with a header and three anchor links elements
+in the nav, include Links from the routing library, directing toward differents paths
+- a header displaying the path of the application
+through a switch, include routes which exclude one another 
+render a simple element in the route to visualize the change in the URL path
+*/
+render() {
+ const toggle=()=>{
+   this.setState({isOpen:!this.state.isOpen});
+ }
+return (
+<div className="App">
 
-export default Navbar;
+  <Navbar className="Navbar" expand="md">
+  <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar >
+    <h2>btf</h2>
+    <ul>
+      <div className="row">
+      <div className="col-md-3">
+      <li>
+        <Link to="/spaces">Spaces</Link>
+      </li>
+      </div>
+      <div className="col-md-3">
+      <li>
+        <Link to="/signs">Signs</Link>
+      </li>
+      </div>
+      <div className="col-md-3">
+      <li>
+        <Link to="/translate">Translator</Link>
+      </li>
+      </div>
+      <div className="col-md-3">
+      <li>
+        <Link to="/practice">Practice</Link>
+      </li>
+      </div>
+      </div>
+    </ul>
+    </Collapse>
+  </Navbar>
+
+  
+
+</div>
+);
+}
+
+}
+
+export default NavBar;
