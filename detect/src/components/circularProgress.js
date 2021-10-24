073329d9b68cbd7334fe39896d5e99bffe3e5bce
Shirin Julka <shirinjlk@gmail.com>
2021-10-24 20:49:16 +0530

    created profile page

--- detect/src/components/circularProgress.js
+++ detect/src/components/circularProgress.js
@@ -5,16 +5,46 @@ import { render } from "react-dom";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
+
+
 import ProgressProvider from "./progressBar";
 
 const App = (props) => {
   const [valueEnd, setValueEnd] = React.useState(props.value);
-  console.log("props",props.value);
+  console.log(props,props.value);
   return (
-    <div style={{ padding: "40px 40px 40px 40px" }}>
+    <div style={{ padding: "30px 30px 30px 30px" }}>
       <Example label="Initial animation upon mount with ProgressProvider wrapper">
         <ProgressProvider valueStart={0} valueEnd={valueEnd}>
-          {value => <CircularProgressbar value={value} text={`${value}%`} />}
+          {value => <CircularProgressbar value={value} text={`${value}%`}    styles={{
+    // Customize the root svg element
+    root: {},
+    // Customize the path, i.e. the "completed progress"
+    path: {
+      // Path color
+      stroke: `rgba(62, 152, 199, ${value})`,
+      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+      strokeLinecap: 'butt',
+      // Customize transition animation
+      transition: 'stroke-dashoffset 0.5s ease 0s',
+    },
+    // Customize the circle behind the path, i.e. the "total progress"
+    trail: {
+      // Trail color
+      stroke: '#d6d6d6',
+    },
+    // Customize the text
+    text: {
+      // Text color
+      fill: '#f88',
+      // Text size
+      fontSize: '10px',
+    },
+    // Customize background - only used when the `background` prop is true
+    background: {
+      fill: '#3e98c7',
+    },
+  }} />}
         </ProgressProvider>
       </Example>
       
@@ -25,13 +55,20 @@ const App = (props) => {
 function Example(props) {
   return (
   
-      <div style={{ marginTop:"3px", marginLeft:"40%"}}>
-        <div style={{ width: 200 }}>{props.children}</div>
-        <div style={{ marginLeft:60 }}>
+      <div style={{ marginTop:"3px"}}>
+        <h1>My Progress</h1>
+        <br/>
+        <br/>
+        <div style={{  marginLeft:550 ,width: 350 }}>{props.children}</div>
+        
+        <div style={{ marginLeft:20 }}>
+          <br/>
+          
         </div>
       </div>
     
   );
 }
-
 export default App;
+
+
